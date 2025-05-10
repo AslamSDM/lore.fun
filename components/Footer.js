d640ef8ad30a0476183b5112fda0bdb197f28306
qnxdev <qnxdev@outlook.com>
2025-05-10 21:38:13 +0530

    updates

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 border-t border-gray-700 py-8">
+    <footer className="bg-[#b99be680] py-8">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "../components/ui/button";
+
 export default function Home({ supabaseClient }) {
   const [featuredStory, setFeaturedStory] = useState(null);
   const [stories, setStories] = useState([]);
@@ -71,14 +73,21 @@ export default function Home({ supabaseClient }) {
 
       <main className="container mx-auto px-4 py-8">
         <section className="mb-16">
-          <div className="text-center mb-12">
+          <div className="text-center mt-20 mb-20">
             <h1 className="text-4xl md:text-6xl font-bold mb-4 text-violet-400 font-medieval">
               LORE.FUN
             </h1>
-            <p className="text-xl md:text-2xl text-gray-300 max-w-3xl mx-auto">
-              Join the first decentralized movie production. Submit storylines,
-              vote on the best content, and bring stories to life.
-            </p>
+            <h2 className="text-4xl md:text-4xl font-bold mb-4 text-violet-400 font-medieval2">
+              Collaborative Storytelling on the Blockchain
+            </h2>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+              <Button size="lg" asChild>
+                <Link href="/story">Read Current Story</Link>
+              </Button>
+              <Button size="lg" variant="outline" asChild>
+                <Link href="/auth">Connect Wallet to Participate</Link>
+              </Button>
+            </div>
           </div>
 
           {loading ? (
