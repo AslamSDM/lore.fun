1c46eb7b63cdc749e31e282cdc231e65495827b6
AslamSDM <aslamprpd@gmail.com>
2025-05-20 14:30:24 +0530

    fix footer zindex

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="border-t border-primary/20 py-10 backdrop-blur-md bg-background/20 relative z-10">
+    <footer className="border-t border-primary/20 py-10 backdrop-blur-md bg-background/20 relative z-[10">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 mb-8">
           <div className="space-y-4">
